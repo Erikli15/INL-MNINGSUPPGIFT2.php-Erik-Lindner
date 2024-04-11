0fd86d5232c3dba4f7216139d721e3afb29306df
Erikli15 <erik.lindner@workmail.com>
2024-04-11 15:24:15 +0200

    css

--- src/Pages/product.php
+++ src/Pages/product.php
@@ -18,24 +18,34 @@ $product = $database->getProduct($id);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" href="src/css/product.css">
 
     <title>Erik O`Company</title>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<?PHP headerLayout($database) ?>
 
 <body>
-    <section class="card" style="width: 20rem;">
-        <article class="card-body">
-            <h3 class="card-title">
-                <?php echo $product->productName ?>
-            </h3>
-            <p class="card-text">
-                <?php echo $product->description ?>
-            </p>
-        </article>
-    </section>
+    <?PHP headerLayout($database) ?>
+    <main>
+        <div class="card mb-3 d-flex" id="product" style="max-width: 540px;">
+            <section class="row g-0">
+                <span class="col-md-4">
+                    <img src="<?php echo $product->imgUrl ?>" class="card-img-top" alt="...">
+                </span>
+                <span class="col-md-8">
+                    <article class="card-body">
+                        <h3 class="card-title">
+                            <?php echo $product->productName ?>
+                        </h3>
+                        <p class="card-text">
+                            <?php echo $product->description ?>
+                        </p>
+                    </article>
+                </span>
+            </section>
+        </div>
+    </main>
 </body>
 
 </html>
