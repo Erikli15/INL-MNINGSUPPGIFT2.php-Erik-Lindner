d8f69425c190857112b77305aa61ce0886d6c1c3
Erikli15 <erik.lindner@workmail.com>
2024-04-26 09:54:00 +0200

    update admin

--- src/Pages/admin.php
+++ src/Pages/admin.php
@@ -4,10 +4,24 @@ require_once ("src/Pages/layouts/aside.php");
 require_once ("src/models/Databas.php");
 $database = new Databas();
 $sortOrder = $_GET['sortOrder'] ?? "";
+$sortOrder = $sortOrder == 'desc' ? 'desc' : 'asc';
 $sortCol = $_GET["sortCol"] ?? "";
 $pageNo = intval($_GET['pageNo'] ?? "1");
-$pageSize = intval($_GET['pageSize'] ?? "100");
+$pageSize = intval($_GET['paSgeSize'] ?? "100");
 $q = $_GET["q"] ?? "";
+
+function oneOf($sortCol, $arrayOfValid, $default)
+{
+    foreach ($arrayOfValid as $a) {
+        if (strcasecmp($a, $sortCol) == 0) {
+            return $a;
+        }
+    }
+    return $default;
+}
+$sortCol = oneOf($sortCol, ["productName", "price", "categoryId", "id"], "id");
+
+
 if ($database->getUserDatabas()->getAuth()->hasRole(\Delight\Auth\Role::ADMIN) == false) {
     header("Location: /user/login");
     exit;
